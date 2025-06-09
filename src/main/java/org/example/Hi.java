23f2e6391016da332ef39fe581f396d4b433773a
Minrui Dong <1366402265@qq.com>
2025-06-09 17:07:19 +0800

    第二次上传

--- src/main/java/org/example/Hi.java
+++ src/main/java/org/example/Hi.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Hi {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello world_1");
+    }
 }
